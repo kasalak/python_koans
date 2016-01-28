eb3318f34e743526133f75497f620c173f8cf414
Karthik <karthik.kasala@gmail.com>
2016-01-28 15:54:37 -0500

    completed 2 koans

--- python3/koans/about_asserts.py
+++ python3/koans/about_asserts.py
@@ -14,13 +14,13 @@ class AboutAsserts(Koan):
         #
         #   http://bit.ly/about_asserts
 
-        self.assertTrue(False) # This should be true
+        self.assertTrue(True) # This should be true
 
     def test_assert_with_message(self):
         """
         Enlightenment may be more easily achieved with appropriate messages.
         """
-        self.assertTrue(False, "This should be true -- Please fix this")
+        self.assertTrue(True, "This should be true -- Please fix this")
 
     def test_fill_in_values(self):
         """
@@ -75,4 +75,3 @@ class AboutAsserts(Koan):
         # Need an illustration? More reading can be found here:
         #
         #   http://bit.ly/__class__
-
