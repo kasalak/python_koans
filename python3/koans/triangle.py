52ffef8c02a071e8f9bc2eefd803e637e443f6f7
Karthik <karthik.kasala@gmail.com>
2016-02-15 13:28:04 -0500

    completed triangle pt 2 lesson

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,14 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    if a==b==c:
+    # DELETE 'PASS' AND WRITE THIS
+    args=sorted([a, b, c])          #use args to be able to sort a, b, c 
+
+    if args[0] <=0:
+        raise TriangleError
+    if args[0] + args[1] < args[2]:
+        raise TriangleError
+    elif a==b==c:
         return 'equilateral'
     elif a==b or b==c or a==c:
         return 'isosceles'
